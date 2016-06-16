666eafb4bb4bdc9eb35de87d72aa611f60a032da
Zia Khan <ziaukhan@hotmail.com>
2016-06-16 18:58:43 +0500

    updated step 0

---
+++ step00_quick_start/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export class App extends Component {
+  render(){
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+
--- step00_quick_start/webpack.config.js
+++ step00_quick_start/webpack.config.js
@@ -5,7 +5,7 @@ var webpack = require('webpack');
  */
 var config = {
   devtool: 'eval-source-map',
-  entry:  __dirname + "/app/App.js",
+  entry:  __dirname + "/app/main.js",
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
